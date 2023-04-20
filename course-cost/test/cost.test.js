fc6fe5ef21a5474f9e61e40ad594f6141efb05dc
bheka <bheka.bj@gmail.com>
2023-04-20 16:31:33 +0200

    last

---
+++ course-cost/test/cost.test.js
@@ -0,0 +1,26 @@
+describe("courseCost function", function() {
+
+    it("should return give 40% discount for course starting between the 20 and 25th of the month", function(){
+        const pythonDiscount = courseCost("p1", "2023-05-21")
+        assert.equal(3140, pythonDiscount.discount) // 
+    });
+    it("should return give 20% discount for a course starting between the 10th and 15th of the month", function(){
+        const PHPDiscount = courseCost("p2", "2023-05-13")
+        assert.equal(1798, PHPDiscount.discount) // 
+    });
+    it("should return 7850 for python course", function(){
+        const cost = courseCost("p1", "2023-05-21")
+        assert.equal(7850, cost.cost) // 
+    });
+    it("should return 8990 for Intro to PHP course", function(){
+        const cost = courseCost("p2", "2023-05-21")
+        assert.equal(8990, cost.cost) // 
+    });
+    it("should return 5675 for python course", function(){
+        const cost = courseCost("r1", "2023-05-21")
+        assert.equal(5675, cost.cost) // 
+    });
+   
+   
+
+})
